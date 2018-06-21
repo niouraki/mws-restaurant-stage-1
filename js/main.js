9deb351fcb98f04bf6fc56f0b5d6664d4cb90d8a
Charikleia Niouraki <niouraki@gmail.com>
2018-06-21 12:06:13 +0300

    Add basic accessibility

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name} restaurant`;
   li.append(image);
 
   const name = document.createElement('h1');
@@ -176,6 +177,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
+  more.role = 'button';
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
