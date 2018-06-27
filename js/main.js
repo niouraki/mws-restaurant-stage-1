3fb41335aef8c187bb8748b8d1f14d036cd54b39
Charikleia Niouraki <niouraki@gmail.com>
2018-06-27 10:46:14 +0300

    Add aria label to buttons

--- js/main.js
+++ js/main.js
@@ -180,6 +180,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.role = 'button';
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', 'View Details of ' + restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
